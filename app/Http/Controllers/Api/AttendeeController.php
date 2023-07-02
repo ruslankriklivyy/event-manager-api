ac4c6be4a90cbb17a04fd0e531b447c774a2b6b0
Ruslan Kriklivyy <ruslan@quartsoft.com>
2023-07-02 17:49:14 +0300

    feat: attendees, events, users and authorization

---
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\AttendeeResource;
+use App\Models\Attendee;
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class AttendeeController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except(['index', 'show', 'update']);
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Event $event)
+    {
+        $attendees = $event->attendees()->latest();
+
+        return AttendeeResource::collection(
+            $attendees->paginate()
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, Event $event)
+    {
+        $attendee = $event->attendees()->create([
+            'user_id' => $request->user()->id
+        ]);
+
+        return new AttendeeResource($attendee);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Event $event, Attendee $attendee)
+    {
+        return new AttendeeResource($attendee);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $event, Attendee $attendee)
+    {
+        $this->authorize('delete-attendee', [$event, $attendee]);
+        $attendee->delete();
+
+        return response(status: 204);
+    }
+}
---
+++ app/Http/Controllers/Api/EventController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
+use App\Http\Traits\CanLoadRelationships;
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    use CanLoadRelationships;
+
+    private array $relations = ['user', 'attendees', 'attendees.user'];
+
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except(['index', 'show']);
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $query = $this->loadRelationships(Event::query());
+
+        return EventResource::collection($query->latest()->paginate());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $event = Event::create([
+            ...$request->validate([
+                'name' => 'required|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'required|date',
+                'end_time' => 'required|date|after:start_date'
+            ]),
+            'user_id' => $request->user()->id
+        ]);
+
+        return new EventResource($this->loadRelationships($event));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Event $event)
+    {
+        return new EventResource($this->loadRelationships($event));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Event $event)
+    {
+        $this->authorize('update-event', $event);
+
+        $event->update([
+            ...$request->validate([
+                'name' => 'sometimes|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'sometimes|date',
+                'end_time' => 'sometimes|date|after:start_date'
+            ])
+        ]);
+
+        return new EventResource($this->loadRelationships($event));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Event $event)
+    {
+        $event->delete();
+
+        return response(status: 204);
+    }
+}
